885d4468b6a4ac3e334ad83de4fb9ca692967088
arjansunar <arjan.gahatrajsunar@gmail.com>
2024-11-30 20:16:15 +0545

    feat: use generics

--- cmd/combinators/main.go
+++ cmd/combinators/main.go
@@ -6,13 +6,13 @@ type Parser interface {
 	parse(input string) string
 }
 
-type ParseState struct {
-	Payload   interface{}
+type ParseState[T any] struct {
+	Payload   T
 	Err       *error
 	Remaining []rune
 }
 
-func (ps ParseState) String() string {
+func (ps ParseState[T]) String() string {
 	errStr := "No Error"
 	if ps.Err != nil {
 		errStr = (*ps.Err).Error()
@@ -20,27 +20,27 @@ func (ps ParseState) String() string {
 	return fmt.Sprintf("ParseState{\n\tPayload: %v,\n\tErr: %v,\n\tRemaining: %q}", ps.Payload, errStr, string(ps.Remaining))
 }
 
-type Func func(input []rune) ParseState
+type Func[T any] func(input []rune) ParseState[T]
 
-func Success(payload interface{}, remaining []rune) ParseState {
-	return ParseState{
+func Success[T any](payload T, remaining []rune) ParseState[T] {
+	return ParseState[T]{
 		Payload:   payload,
 		Remaining: remaining,
 	}
 }
 
-func Err(err *error, remaining []rune) ParseState {
-	return ParseState{
+func Err[T any](err *error, remaining []rune) ParseState[T] {
+	return ParseState[T]{
 		Err:       err,
 		Remaining: remaining,
 	}
 }
 
-func Char(char rune) Func {
-	return func(input []rune) ParseState {
+func Char(char rune) Func[string] {
+	return func(input []rune) ParseState[string] {
 		if len(input) == 0 || input[0] != char {
 			err := fmt.Errorf("expected %v got %v", char, string(input))
-			return Err(&err, input)
+			return Err[string](&err, input)
 		}
 
 		return Success(string(char), input[1:])
@@ -50,5 +50,6 @@ func Char(char rune) Func {
 func main() {
 	println("Trying to parse a character")
 	dollarParser := Char('$')
-	println(dollarParser([]rune("$Testing")).String())
+	res := dollarParser([]rune("$Testing"))
+	println(res.Payload)
 }
